8fa1f2a433271bdfb85b544c03176f9db1a95101
Mark Rattle <mark.rattle@gmail.com>
2022-09-25 18:00:05 -0400

    running loop to add to checklist

--- checklist.py
+++ checklist.py
@@ -1,4 +1,5 @@
 checklist = []
+
 # checklist.append('Blue')
 # print(checklist)
 # checklist.append('Orange')
@@ -27,6 +28,58 @@ def update(index, item):
 def destroy(index):
     checklist.pop(index)
 
+def list_all_items():
+    index = 0
+    for list_item in checklist:
+        print(f'{str(index)} {list_item}')
+        index += 1
+
+def mark_completed():
+    index = 0
+    for list_item in checklist:
+        checklist[index] = '√ ' + list_item
+        print(checklist[index])
+        index += 1
+
+def user_input(prompt):
+    # the input function will display a message in the terminal and wait
+    # for user input
+    user_input = input(prompt)
+    return user_input
+
+def select(function_code):
+    # create item
+    if function_code.lower() == "c":
+        input_item = user_input("Input item > ")
+        create(input_item)
+        return True
+
+    # read item      
+    elif function_code.lower() == "r":
+        item_index = int(user_input("Index Number? > "))
+        while item_index >= len(checklist):
+            item_index = int(input(f"Not a valid index number, please enter a number from 0 - {len(checklist) -1} > "))
+        print(f"{item_index} {read(item_index)}")
+        return True
+
+    # print all items
+    elif function_code.lower() == "p":
+        if len(checklist) > 0:
+            list_all_items()
+        else:
+            print("You haven't added anything to the checklist yet.")
+        return True
+
+    # exit/quit    
+    elif function_code.lower() == "q":
+        print("Done!")
+        return False
+
+    # catch all
+    else:
+        print("Unknown Option")
+        return True
+    
 def test():
     create("purple sox")
     create("red cloak")
@@ -38,5 +91,18 @@ def test():
     destroy(1)
 
     print(read(0))
+    mark_completed()
 
-test()    
+    list_all_items()
+
+    select("C")
+    list_all_items()
+    select("R")
+    list_all_items()
+
+# test()
+
+running = True
+while running:
+    selection = user_input("Press C to add to list, R to Read from list, P to display list or Q to quit > ")
+    running = select(selection)
